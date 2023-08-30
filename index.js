2b226a4ee44c0c38c5c2c9bd7e721083f11f05e6
Your Name <you@example.com>
2023-08-30 16:03:00 +0500

    latest and updated commit

--- index.js
+++ index.js
@@ -2,15 +2,23 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(cors());
 
 app.get('/', (req, res) => {
-    res.status(200).send('Hello World!');
+    res.status(200).send('Hello World!!!');
+});
+
+app.get('/products', (req, res) => {
+    res.status(200).send('All Products!!!');
+});
+
+app.get('/allProducts', (req, res) => {
+    res.status(200).send("Hello World I got all Products!!!");
 });
 
 app.listen(port, () => {
-    console.log(`Server is Listening at the port ${PORT}`);
+    console.log(`Server is Listening at the port ${port}`);
 });
