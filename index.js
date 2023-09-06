dbb43e4fc9b14a3168fb49c932c37150c28f2354
Your Name <you@example.com>
2023-09-07 01:50:26 +0500

    most most updated commit

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const PORT = 8080;
 
 
 app.use(express.json());
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+app.use(cors());
 
 app.use("/", router);
 app.use("/products", productRouter);
