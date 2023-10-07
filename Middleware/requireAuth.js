888a9ad0b477199af4927cb84bff5d656406460c
Your Name <you@example.com>
2023-10-08 02:41:16 +0500

    latest commits

--- Middleware/requireAuth.js
+++ Middleware/requireAuth.js
@@ -4,19 +4,15 @@ const pool = require("./dbConnect");
 const requireAuth = async (req, res, next) => {
   const { authorization } = req.headers;
   if (!authorization) {
-    return res.status(401).json({ error: "Authorization not required!" });
+    return res.status(401).json({ error: "Authorization Required!" });
   }
   const token = authorization;
   try {
     const query = "SELECT * FROM users WHERE email = ?";
     const { email } = jwt.verify(token, process.env.SECRET);
     pool.query(query, [email], async (results, error) => {
-      if (!results) {
-        res.json(error);
-      } else {
         req.user = results;
         next();
-      }
     });
   } catch (error) {
     console.log(error);
